6197e490331f1f5ca2ee412dce33a0be281534b7
kate! <kate@dariox.club>
2022-09-29 15:50:27 +0800

    Add field PasswordHash

--- OpenSoftwareLauncher.Shared/Authentication/User.cs
+++ OpenSoftwareLauncher.Shared/Authentication/User.cs
@@ -7,6 +7,7 @@ namespace OpenSoftwareLauncher.Shared.Authentication
     public class User
     {
         public string Username { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
         public long CreatedTimestamp { get; set; } = 0;
 
         public string[] PackageIds { get; set; } = Array.Empty<string>();
