b44e7a52053dd404d3fc5419eaea2973c95b208d
kate! <kate@dariox.club>
2022-10-04 17:50:40 +0800

    Create Delegates.cs

---
+++ OpenSoftwareLauncher.DesktopWinForms/Delegates.cs
@@ -0,0 +1,11 @@
+﻿using OpenSoftwareLauncher.DesktopWinForms.ServerBridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSoftwareLauncher.DesktopWinForms
+{
+    public delegate void ContentFieldDelegate(ContentField field);
+}
