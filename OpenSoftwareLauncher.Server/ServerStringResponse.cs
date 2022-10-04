7e2fc2e79cee344b8a70c87b23c1ff7fc5102533
kate! <kate@dariox.club>
2022-10-04 17:50:58 +0800

    Response with locale keys

--- OpenSoftwareLauncher.Server/ServerStringResponse.cs
+++ OpenSoftwareLauncher.Server/ServerStringResponse.cs
@@ -4,20 +4,20 @@ namespace OpenSoftwareLauncher.Server
 {
     public static class ServerStringResponse
     {
-        public static string InvalidCredential = "Invalid Credential";
-        public static string InvalidPermission = "Invalid Permissions";
-        public static string UnsupportedMediaType = "Unsupported Media Type";
+        public static string InvalidCredential = "ServerResponse_InvalidCredentials";
+        public static string InvalidPermission = "ServerResponse_InvalidPermission";
+        public static string UnsupportedMediaType = "ServerResponse_MediaTypeUnsupported";
         public static string AccountNotFound(string username)
             => $"Could not find account with username of \"{username}\"";
 
         public static string InvalidParameter(string parameterName)
             => $"Invalid parameter \"{parameterName}\"";
-        public static string InvalidBody => "Invalid Body";
+        public static string InvalidBody => "ServerResponse_InvalidBody";
 
         public static string ExpectedValueOnProperty(string propertyName, object expectedValue, object recievedValue)
             => $"Expected {propertyName} to be \"{JsonSerializer.Serialize(expectedValue)}\" but got \"{JsonSerializer.Serialize(recievedValue)}\" instead";
 
-        public static string AccountDisabled => $"Your account is disabled. Please contact licensing@minalyze.com";
-        public static string SerializationFailure => $"Failed to serialize data";
+        public static string AccountDisabled => $"ServerResponse_AccountDisabled";
+        public static string SerializationFailure => $"ServerResponse_BodySerializationFailure";
     }
 }
