46a43b548faf1adcdff3823a6cc686bac0a38f77
kate! <kate@dariox.club>
2022-10-04 17:51:29 +0800

    Change namespace

--- OpenSoftwareLauncher.DesktopWinForms/ServerBridge/AuthClient.cs
+++ OpenSoftwareLauncher.DesktopWinForms/ServerBridge/AuthClient.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenSoftwareLauncher.DesktopWinForms
+namespace OpenSoftwareLauncher.DesktopWinForms.ServerBridge
 {
     public class AuthClient
     {
