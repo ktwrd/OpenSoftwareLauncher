39a69a910d099880596566fff32db1d0e5d774d2
kate <kate@dariox.club>
2022-10-14 16:31:42 +0800

    Create UserDisable Form

---
+++ OpenSoftwareLauncher.DesktopWinForms/User Management/UserDisableForm.cs
@@ -0,0 +1,50 @@
+﻿using OSLCommon.Authorization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSoftwareLauncher.DesktopWinForms
+{
+    public partial class UserDisableForm : Form
+    {
+        public UserDisableForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Locale()
+        {
+            Text = LocaleManager.Get("FieldBanish") + $" - {Account.Username}";
+            buttonSubmit.Text = LocaleManager.Get("Submit");
+        }
+
+        public AccountDetailsResponse Account;
+        public UserDisableForm(AccountDetailsResponse account)
+        {
+            Account = account;
+            InitializeComponent();
+            Locale();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            Enabled = false;
+            var response = Program.Client.AccountBan(Account.Username, textBoxReason.Text).Result;
+            if (response != null)
+            {
+                MessageBox.Show(LocaleManager.Get(response.Message, @"Failed to ban user"));
+            }
+            if (Program.ClientContext.ParentForm != null && Program.ClientContext.ParentForm.UserManagementForm != null)
+            {
+                Program.ClientContext.ParentForm.UserManagementForm.ReloadList(false);
+            }
+            Close();
+        }
+    }
+}
