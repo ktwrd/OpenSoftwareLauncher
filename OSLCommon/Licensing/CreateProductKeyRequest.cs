95952e65795fafb6e20dc804320beeb80815b55d
kate <kate@dariox.club>
2022-10-25 15:47:34 +0800

    Create class CreateProductKeyRequest

---
+++ OSLCommon/Licensing/CreateProductKeyRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Licensing
+{
+    public class CreateProductKeyRequest
+    {
+        public int Count { get; set; }
+        public string[] RemoteLocations { get; set; }
+        public long ExpiryTimestamp { get; set; }
+        public string Note { get; set; }
+        public string GroupLabel { get; set; }
+    }
+}
