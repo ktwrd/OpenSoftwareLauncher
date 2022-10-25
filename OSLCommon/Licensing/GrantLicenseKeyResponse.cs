385174833c315de16ac66e51cb3901ca25b45ec6
kate <kate@dariox.club>
2022-10-25 15:47:23 +0800

    Create class GrantLicenseKeyResponse

---
+++ OSLCommon/Licensing/GrantLicenseKeyResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Licensing
+{
+    public enum GrantLicenseKeyResponseCode
+    {
+        Invalid,
+        AlreadyRedeemed,
+        Granted
+    }
+    public class GrantLicenseKeyResponse
+    {
+        public GrantLicenseKeyResponseCode Code { get; set; }
+        public string[] Products { get; set; }
+    }
+}
