8ecc6181a234606dd35e2587328b42027bb1e5f8
kate <kate@dariox.club>
2022-10-25 15:54:28 +0800

    Create Licence Key Creation Dialog

---
+++ OpenSoftwareLauncher.DesktopWinForms/LicenseKeyCreateDialog.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace OpenSoftwareLauncher.DesktopWinForms
+{
+    partial class LicenseKeyCreateDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LicenseKeyCreateDialog));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownLicenseCount = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNote = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listBoxRelease = new System.Windows.Forms.ListBox();
+            this.comboBoxReleaseSelect = new System.Windows.Forms.ComboBox();
+            this.buttonPush = new System.Windows.Forms.Button();
+            this.buttonReleaseAdd = new System.Windows.Forms.Button();
+            this.buttonReleaseRemove = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLicenseCount)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownLicenseCount, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxNote, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxName, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 198);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(255, 79);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(289, 82);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Count";
+            // 
+            // numericUpDownLicenseCount
+            // 
+            this.numericUpDownLicenseCount.Dock = System.Windows.Forms.DockStyle.Top;
+            this.numericUpDownLicenseCount.Location = new System.Drawing.Point(78, 4);
+            this.numericUpDownLicenseCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownLicenseCount.Name = "numericUpDownLicenseCount";
+            this.numericUpDownLicenseCount.Size = new System.Drawing.Size(207, 20);
+            this.numericUpDownLicenseCount.TabIndex = 1;
+            this.numericUpDownLicenseCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Note";
+            // 
+            // textBoxNote
+            // 
+            this.textBoxNote.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxNote.Location = new System.Drawing.Point(78, 31);
+            this.textBoxNote.Name = "textBoxNote";
+            this.textBoxNote.Size = new System.Drawing.Size(207, 20);
+            this.textBoxNote.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Group Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxName.Location = new System.Drawing.Point(78, 58);
+            this.textBoxName.MaxLength = 100;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(207, 20);
+            this.textBoxName.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.buttonReleaseAdd, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listBoxRelease, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.comboBoxReleaseSelect, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 6);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(289, 186);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.buttonReleaseRemove);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(242, 1);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(46, 154);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // listBoxRelease
+            // 
+            this.listBoxRelease.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxRelease.FormattingEnabled = true;
+            this.listBoxRelease.Location = new System.Drawing.Point(4, 4);
+            this.listBoxRelease.Name = "listBoxRelease";
+            this.listBoxRelease.Size = new System.Drawing.Size(234, 148);
+            this.listBoxRelease.TabIndex = 4;
+            // 
+            // comboBoxReleaseSelect
+            // 
+            this.comboBoxReleaseSelect.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBoxReleaseSelect.FormattingEnabled = true;
+            this.comboBoxReleaseSelect.Location = new System.Drawing.Point(4, 160);
+            this.comboBoxReleaseSelect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 3);
+            this.comboBoxReleaseSelect.Name = "comboBoxReleaseSelect";
+            this.comboBoxReleaseSelect.Size = new System.Drawing.Size(234, 21);
+            this.comboBoxReleaseSelect.TabIndex = 5;
+            this.comboBoxReleaseSelect.SelectedIndexChanged += new System.EventHandler(this.comboBoxReleaseSelect_SelectedIndexChanged);
+            // 
+            // buttonPush
+            // 
+            this.buttonPush.Image = global::OpenSoftwareLauncher.DesktopWinForms.Properties.Resources.upload_cloud;
+            this.buttonPush.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonPush.Location = new System.Drawing.Point(6, 286);
+            this.buttonPush.Name = "buttonPush";
+            this.buttonPush.Size = new System.Drawing.Size(75, 23);
+            this.buttonPush.TabIndex = 2;
+            this.buttonPush.Text = "Push";
+            this.buttonPush.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonPush.UseVisualStyleBackColor = true;
+            this.buttonPush.Click += new System.EventHandler(this.buttonPush_Click);
+            // 
+            // buttonReleaseAdd
+            // 
+            this.buttonReleaseAdd.Enabled = false;
+            this.buttonReleaseAdd.Image = global::OpenSoftwareLauncher.DesktopWinForms.Properties.Resources.plus;
+            this.buttonReleaseAdd.Location = new System.Drawing.Point(245, 159);
+            this.buttonReleaseAdd.Name = "buttonReleaseAdd";
+            this.buttonReleaseAdd.Size = new System.Drawing.Size(40, 23);
+            this.buttonReleaseAdd.TabIndex = 3;
+            this.buttonReleaseAdd.UseVisualStyleBackColor = true;
+            this.buttonReleaseAdd.Click += new System.EventHandler(this.buttonReleaseAdd_Click);
+            // 
+            // buttonReleaseRemove
+            // 
+            this.buttonReleaseRemove.Enabled = false;
+            this.buttonReleaseRemove.Image = global::OpenSoftwareLauncher.DesktopWinForms.Properties.Resources.minus;
+            this.buttonReleaseRemove.Location = new System.Drawing.Point(3, 128);
+            this.buttonReleaseRemove.Name = "buttonReleaseRemove";
+            this.buttonReleaseRemove.Size = new System.Drawing.Size(40, 23);
+            this.buttonReleaseRemove.TabIndex = 2;
+            this.buttonReleaseRemove.UseVisualStyleBackColor = true;
+            this.buttonReleaseRemove.Click += new System.EventHandler(this.buttonReleaseRemove_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonPush, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel1, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(301, 315);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // LicenseKeyCreateDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 315);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(317, 354);
+            this.Name = "LicenseKeyCreateDialog";
+            this.Text = "Create Licence Keys";
+            this.Shown += new System.EventHandler(this.LicenseKeyCreateDialog_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLicenseCount)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownLicenseCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNote;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonReleaseAdd;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonReleaseRemove;
+        private System.Windows.Forms.ComboBox comboBoxReleaseSelect;
+        private System.Windows.Forms.Button buttonPush;
+        private System.Windows.Forms.ListBox listBoxRelease;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
---
+++ OpenSoftwareLauncher.DesktopWinForms/LicenseKeyCreateDialog.cs
@@ -0,0 +1,76 @@
+﻿using OSLCommon.Licensing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSoftwareLauncher.DesktopWinForms
+{
+    public partial class LicenseKeyCreateDialog : Form
+    {
+        public LicenseKeyCreateDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateEnableState()
+        {
+            buttonReleaseAdd.Enabled = comboBoxReleaseSelect.SelectedIndex > 0;
+            buttonReleaseRemove.Enabled = listBoxRelease.SelectedIndex >= 0;
+        }
+        public void ResetComboBox()
+        {
+            comboBoxReleaseSelect.Items.Clear();
+            comboBoxReleaseSelect.Items.Add("");
+            comboBoxReleaseSelect.SelectedIndex = 0;
+            foreach (var i in Program.LocalContent.GetRemoteLocations())
+                if (!listBoxRelease.Items.Contains(i))
+                    comboBoxReleaseSelect.Items.Add(i);
+            UpdateEnableState();
+        }
+        private void buttonReleaseAdd_Click(object sender, EventArgs e)
+        {
+            listBoxRelease.Items.Add(comboBoxReleaseSelect.SelectedItem.ToString());
+            ResetComboBox();
+            UpdateEnableState();
+        }
+        private void buttonPush_Click(object sender, EventArgs e)
+        {
+            string[] streams = new string[listBoxRelease.Items.Count];
+            for (int i = 0; i < streams.Length; i++)
+                streams[i] = listBoxRelease.Items[i].ToString();
+            var data = new CreateProductKeyRequest
+            {
+                Count = int.Parse(numericUpDownLicenseCount.Value.ToString()),
+                RemoteLocations = streams,
+                ExpiryTimestamp = 0,
+                Note = textBoxNote.Text,
+                GroupLabel = textBoxName.Text
+            };
+
+            var res = Program.Client.CreateLicenseKeys(data);
+            Console.WriteLine();
+        }
+
+        private void buttonReleaseRemove_Click(object sender, EventArgs e)
+        {
+            listBoxRelease.Items.Remove(listBoxRelease.SelectedItem);
+            ResetComboBox();
+        }
+
+        private void LicenseKeyCreateDialog_Shown(object sender, EventArgs e)
+        {
+            ResetComboBox();
+        }
+
+        private void comboBoxReleaseSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateEnableState();
+        }
+    }
+}
