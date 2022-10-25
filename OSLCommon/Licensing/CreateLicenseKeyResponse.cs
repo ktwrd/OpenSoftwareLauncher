ad3d603df5714e9d05a93941839e90c3c13fc271
kate <kate@dariox.club>
2022-10-25 15:47:45 +0800

    Create class CreateLicenseKeyResponse

---
+++ OSLCommon/Licensing/CreateLicenseKeyResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Licensing
+{
+    public class CreateLicenseKeyResponse
+    {
+        public LicenseKeyMetadata[] Keys { get; set; }
+        public string GroupId { get; set; }
+    }
+}
