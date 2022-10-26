00199c5fd96861d2156ead4b30b4135a3db99c16
kate! <kate@dariox.club>
2022-10-26 20:51:53 +0800

    Update BaseContentManager.cs

--- OpenSoftwareLauncher.Server/BaseContentManager.cs
+++ OpenSoftwareLauncher.Server/BaseContentManager.cs
@@ -12,32 +12,65 @@ namespace OpenSoftwareLauncher.Server
         internal abstract Task Deserialize();
         internal abstract Task Serialize();
 
+        /// <summary>
+        /// <see cref="AccountManager.AccountList"/>
+        /// </summary>
+        /// <returns></returns>
         public abstract Task<Account[]> GetAccount();
+        /// <summary>
+        /// <see cref="AccountManager.GetAccountByUsername(string)"/>
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
         public abstract Task<Account> GetAccountByUsername(string username);
-        public abstract Task<Account> GetAccountByToken(string token);
+        /// <summary>
+        /// <see cref="AccountManager.GetAccount(string, bool)"/>
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public abstract Task<Account> GetAccountByToken(string token, bool bumpLastUsed=false);
 
         #region Token
+        /// <summary>
+        /// <see cref="AccountManager.AccountHasPermission(string, AccountPermission[], bool, bool)"/>
+        /// </summary>
         public abstract Task<bool> AccountHasPermission(
             string username,
             AccountPermission[] permissions,
             bool ignoreAdmin = false);
+        /// <summary>
+        /// <see cref="AccountManager.TokenMarkLastUsedTimestamp(AccountToken)"/>
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         internal abstract Task<AccountToken> TokenMarkLastUsedTimestamp(
             string token);
+        /// <summary>
+        /// <see cref="AccountManager.ValidateToken(string)"/>
+        /// </summary>
         public abstract Task<bool> TokenExists(string token);
-        #endregion
+        /// <summary>
+        /// <see cref="AccountManager.TokenUsed(string)"/>
+        /// </summary>
+        public abstract Task TokenUsed(string token);
+        /// <summary>
+        /// <see cref="AccountManager.GrantTokenAndOrAccount(string, string, string, string)"/>
+        /// </summary>
         public abstract Task<GrantTokenResponse> AccountGrantToken(
             string username,
             string password,
             string userAgent = "",
             string host = "",
             bool createAccountIfNotInDatabase = true);
+        #endregion
         #region License
+        /// <summary>
+        /// <see cref=""/>
+        /// </summary>
         public abstract Task AccountLicenseGrant(string username, string product);
         public abstract Task AccountLicenseGrant(Dictionary<string, string[]> dict);
         public abstract Task AccountLicenseRevoke(string username, string product);
         public abstract Task AccountLicenseRevoke(Dictionary<string, string[]> dict);
         #endregion
-
-        public abstract Task SetAccount(Account account);
     }
 }
