1a5a1622742c41cb75d5da698574d614fb57f2d4
kate <kate@dariox.club>
2022-10-26 10:33:02 +0800

    Add comments

--- OSLCommon/Licensing/GrantLicenseKeyResponse.cs
+++ OSLCommon/Licensing/GrantLicenseKeyResponse.cs
@@ -14,7 +14,13 @@ namespace OSLCommon.Licensing
     public class GrantLicenseKeyResponse
     {
         public GrantLicenseKeyResponseCode Code { get; set; }
+        /// <summary>
+        /// Products Granted
+        /// </summary>
         public string[] Products { get; set; }
+        /// <summary>
+        /// Permissions Granted
+        /// </summary>
         public AccountPermission[] Permissions { get; set; }
     }
 }
