62a5e4cf3b9810f654ac9419b21ca2ffca0f2787
kate <kate@dariox.club>
2022-10-27 10:24:08 +0800

    Create enum LicenseField

---
+++ OSLCommon/Licensing/LicenseField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Licensing
+{
+    public enum LicenseField
+    {
+        All,
+        Enable,
+        AllGroups,
+        License,
+    }
+}
