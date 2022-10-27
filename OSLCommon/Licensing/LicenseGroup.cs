6eb08f806ad5765d2f9fc8794a66103e0ef6f01f
kate <kate@dariox.club>
2022-10-27 10:24:15 +0800

    Create class LicenseGroup

---
+++ OSLCommon/Licensing/LicenseGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Licensing
+{
+    public class LicenseGroup
+    {
+        public string UID { get; set; } = "";
+        public string DisplayName { get; set; } = "";
+        public string[] LicenseIds { get; set; } = Array.Empty<string>();
+        public long CreatedTimestamp { get; set; } = 0;
+        public string CreatedBy { get; set; } = "";
+        public string Note { get; set; } = "";
+    }
+}
