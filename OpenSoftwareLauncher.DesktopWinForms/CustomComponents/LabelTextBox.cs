57ca2305a4bc6d312a7399352f6e4bbd1e3ae959
kate <kate@dariox.club>
2022-10-31 14:57:37 +0800

    Create config form

---
+++ OpenSoftwareLauncher.DesktopWinForms/CustomComponents/LabelTextBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSoftwareLauncher.DesktopWinForms.CustomComponents
+{
+    public partial class LabelTextBox : UserControl
+    {
+        public LabelTextBox()
+        {
+            InitializeComponent();
+            label1.Text = labelText;
+            textBox1.Multiline = multiLine;
+        }
+
+        private string labelText;
+        public string LabelText
+        {
+            get
+            {
+                return label1.Text;
+            }
+            set
+            {
+                labelText = value;
+                label1.Text = labelText;
+            }
+        }
+        private bool multiLine;
+        public bool MultiLine
+        {
+            get
+            {
+                return textBox1.Multiline;
+            }
+            set
+            {
+                multiLine = value;
+                textBox1.Multiline = multiLine;
+            }
+        }
+
+        public Label Label => label1;
+        public TextBox TextBox => textBox1;
+    }
+}
