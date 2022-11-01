8cf8311baa25fd976ccba3ebc13a2e28f76d441c
kate! <kate@dariox.club>
2022-11-01 20:52:51 +0800

    Update VersionAutoIncrement.cs

--- OpenSoftwareLauncher.Server/Properties/VersionAutoIncrement.cs
+++ OpenSoftwareLauncher.Server/Properties/VersionAutoIncrement.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.106.477")]
-[assembly: AssemblyFileVersion("1.0.106.477")]
+[assembly: AssemblyVersion("1.0.106.764")]
+[assembly: AssemblyFileVersion("1.0.106.764")]
 
