edd018f3660afbcb161276ae7cbbc4d3e9dd14d2
kate <kate@dariox.club>
2022-11-01 14:40:50 +0800

    Create MongoSystemAnnouncement

---
+++ OSLCommon/MongoSystemAnnouncement.cs
@@ -0,0 +1,89 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OSLCommon
+{
+    public class MongoSystemAnnouncement : SystemAnnouncement
+    {
+        internal MongoClient mongoClient;
+        public MongoSystemAnnouncement(MongoClient client)
+        {
+            mongoClient = client;
+            AnnouncementUpdate += (entry) =>
+            {
+                var collection = GetAnnouncementCollection<SystemAnnouncementEntry>();
+                var filter = Builders<SystemAnnouncementEntry>.Filter.Eq("ID", entry.ID);
+                collection.ReplaceOne(filter, entry);
+            };
+        }
+        public string DatabaseName = "opensoftwarelauncher";
+        public string CollectionName = "announcements";
+        private IMongoCollection<T> GetAnnouncementCollection<T>()
+        {
+            return mongoClient.GetDatabase(DatabaseName).GetCollection<T>(CollectionName);
+        }
+
+        public override SystemAnnouncementEntry GetLatest()
+        {
+            var filter = Builders<SystemAnnouncementEntry>
+                .Filter
+                .Eq("Active", true);
+
+            var exec = GetAnnouncementCollection<SystemAnnouncementEntry>()
+                .Find(filter)
+                .ToEnumerable()
+                .OrderBy(o => o.Timestamp)
+                .ToList();
+
+            SystemAnnouncementEntry target = null;
+            long ts = 0;
+            foreach (var item in exec)
+            {
+                if (item.Timestamp > ts)
+                    target = item;
+            }
+            InjectEntry(target);
+            return target;
+        }
+        public override SystemAnnouncementEntry[] GetAll()
+        {
+            var filter = Builders<SystemAnnouncementEntry>
+                .Filter
+                .Where(v => v.Timestamp > 0);
+            return GetAnnouncementCollection<SystemAnnouncementEntry>()
+                    .Find(filter)
+                    .ToList()
+                    .ToArray();
+        }
+        public override void Set(SystemAnnouncementEntry[] all)
+        {
+            var collection = GetAnnouncementCollection<SystemAnnouncementEntry>();
+            foreach (var item in all)
+            {
+                var filter = Builders<SystemAnnouncementEntry>
+                    .Filter
+                    .Eq("ID", item.ID);
+                var exist = collection.Find(filter).ToList();
+                if (exist.Count < 1)
+                    collection.InsertOne(item);
+                else if (exist.Count > 0)
+                    collection.FindOneAndReplace(filter, item);
+            }
+        }
+        public override void Add(SystemAnnouncementEntry entry)
+        {
+            var collection = GetAnnouncementCollection<SystemAnnouncementEntry>();
+            var filter = Builders<SystemAnnouncementEntry>
+                .Filter
+                .Eq("ID", entry.ID);
+            var exist = collection.Find(filter).ToList().Count > 0;
+            if (exist)
+                collection.FindOneAndReplace(filter, entry);
+            else
+                collection.InsertOne(entry);
+        }
+    }
+}
--- OSLCommon/SystemAnnouncement.cs
+++ OSLCommon/SystemAnnouncement.cs
@@ -1,24 +1,15 @@
 ﻿using kate.shared.Helpers;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace OSLCommon
 {
-    public class SystemAnnouncementEntry
-    {
-        public string Message { get; set; }
-        public long Timestamp { get; set; }
-        public bool Active { get; set; }
-        public SystemAnnouncementEntry()
-        {
-            Message = "";
-            Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-            Active = false;
-        }
-    }
     public class SystemAnnouncementSummary
     {
         public SystemAnnouncementEntry[] Entries { get; set; }
@@ -29,12 +20,88 @@ namespace OSLCommon
             Active = false;
         }
     }
-    public class SystemAnnouncement
+    public delegate void AnnouncementDelegate(SystemAnnouncementEntry announcement);
+
+
+    public class SystemAnnouncementEntry
+    {
+        [BsonIgnore]
+        [JsonIgnore]
+        internal SystemAnnouncement manager;
+        [BsonIgnore]
+        [JsonIgnore]
+        internal bool hooked = false;
+        [JsonIgnore]
+        public ObjectId _id { get; set; }
+        public string Message 
+        {
+            get => message;
+            set
+            {
+                message = value;
+                if (manager != null)
+                    manager.OnAnnouncementUpdate(this);
+            }
+        }
+        internal string message;
+        public long Timestamp
+        {
+            get => timestamp;
+            set
+            {
+                timestamp = value;
+                if (manager != null)
+                    manager.OnAnnouncementUpdate(this);
+            }
+        }
+        internal long timestamp;
+        public bool Active
+        {
+            get => active;
+            set
+            {
+                active = value;
+                if (manager != null)
+                    manager.OnAnnouncementUpdate(this);
+            }
+        }
+        internal bool active;
+        public string ID { get; set; }
+        public SystemAnnouncementEntry(SystemAnnouncement manager)
+        {
+            this.manager = manager;
+            ID = GeneralHelper.GenerateToken(12);
+            Message = "";
+            Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            Active = false;
+        }
+        public SystemAnnouncementEntry() : this(null)
+        { }
+        public void Merge(SystemAnnouncementEntry entry)
+        {
+            active = entry.active;
+            timestamp = entry.timestamp;
+            message = entry.message;
+        }
+    }
+    public partial class SystemAnnouncement
     {
         public List<SystemAnnouncementEntry> Entries = new List<SystemAnnouncementEntry>();
         public bool Active = false;
 
-        public SystemAnnouncementEntry GetLatest()
+        internal void InjectEntry(SystemAnnouncementEntry entry)
+        {
+            if (entry == null || entry.hooked) return;
+            AnnouncementUpdate += (item) =>
+            {
+                if (item.ID == entry.ID)
+                {
+                    entry.Merge(item);
+                }
+            };
+            entry.hooked = true;
+        }
+        public virtual SystemAnnouncementEntry GetLatest()
         {
             Entries = new List<SystemAnnouncementEntry>(Entries.OrderBy(o => o.Timestamp));
             long ts = 0;
@@ -44,27 +111,67 @@ namespace OSLCommon
                 if (item.Timestamp > ts && item.Active)
                     target = item;
             }
+            InjectEntry(target);
             return target;
         }
-
-        public SystemAnnouncementEntry Set(string content, bool active = false)
+        public virtual SystemAnnouncementEntry[] GetAll()
         {
-            var entry = new SystemAnnouncementEntry()
+            return Entries.ToArray();
+        }
+        public virtual void Set(SystemAnnouncementEntry[] all)
+        {
+            var toUpdate = all.Where(t => Entries.Where(v => v.ID == t.ID).Count() == 1).ToList();
+            var newThings = all.Where(t => Entries.Where(v => v.ID == t.ID).Count() < 1).ToList();
+
+            foreach (var item in Entries)
+            {
+                foreach (var thing in toUpdate)
+                {
+                    item.Message = thing.Message;
+                    item.Active = thing.Active;
+                }
+            }
+            foreach (var item in newThings)
+                Entries.Add(item);
+        }
+
+        public virtual SystemAnnouncementEntry Set(string content, bool active = false)
+        {
+            var entry = new SystemAnnouncementEntry(this)
             {
                 Message = content,
                 Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
                 Active = active
             };
+            Add(entry);
+            return entry;
+        }
+        public virtual void Add(SystemAnnouncementEntry entry)
+        {
             Entries.Add(entry);
             OnUpdate();
-            return entry;
+        }
+        public virtual void Add(string content, bool active = false)
+        {
+            Add(new SystemAnnouncementEntry(this)
+            {
+                Message = content,
+                Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                Active = active
+            });
         }
 
         public event VoidDelegate Update;
+        public event AnnouncementDelegate AnnouncementUpdate;
         public void OnUpdate()
         {
             if (Update != null)
                 Update?.Invoke();
+        }
+        public void OnAnnouncementUpdate(SystemAnnouncementEntry entry)
+        {
+            if (AnnouncementUpdate != null)
+                AnnouncementUpdate.Invoke(entry);
         }
 
         public SystemAnnouncementSummary GetSummary()
