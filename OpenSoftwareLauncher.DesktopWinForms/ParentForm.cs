451c00368bfb0e4ca03b732e9455112d96f6d251
kate! <kate@dariox.club>
2022-11-07 12:32:08 +0800

    Fix usage of toolStripButtonSettings/Releases

--- OpenSoftwareLauncher.DesktopWinForms/ParentForm.cs
+++ OpenSoftwareLauncher.DesktopWinForms/ParentForm.cs
@@ -26,14 +26,8 @@ namespace OpenSoftwareLauncher.DesktopWinForms
             toolStripButtonAnnouncements.Text = LocaleManager.Get(toolStripButtonAnnouncements.Text);
             toolStripButtonAnnouncements.ToolTipText = toolStripButtonAnnouncements.Text;
 
-            toolStripButtonReleases.Text = LocaleManager.Get(toolStripButtonReleases.Text);
-            toolStripButtonReleases.ToolTipText = toolStripButtonReleases.Text;
-
             toolStripButtonLicenceManagement.Text = LocaleManager.Get(toolStripButtonLicenceManagement.Text);
             toolStripButtonLicenceManagement.ToolTipText = toolStripButtonLicenceManagement.Text;
-
-            toolStripButtonSettings.Text = LocaleManager.Get(toolStripButtonSettings.Text);
-            toolStripButtonSettings.ToolTipText = toolStripButtonSettings.Text;
 
             toolStripButtonLogs.Text = LocaleManager.Get(toolStripButtonLogs.Text);
             toolStripButtonLogs.ToolTipText = toolStripButtonLogs.Text;
