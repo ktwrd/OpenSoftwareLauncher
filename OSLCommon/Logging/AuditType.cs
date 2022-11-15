ba4cd43f7f6b46d1e27ca6ac4031760337f29691
kate <kate@dariox.club>
2022-11-15 11:35:52 +0800

    Create enum AuditType

---
+++ OSLCommon/Logging/AuditType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Logging
+{
+    public enum AuditType
+    {
+        None,
+        
+        AccountModify           = 100,
+        AccountDisable          = 101,
+        AccountPermissionGrant  = 120,
+        AccountPermissionRevoke = 121,
+        LicenseRedeem           = 130,
+        TokenDelete             = 140,
+        TokenCreate             = 142,
+
+        AnnouncementModify      = 200,
+        AnnouncementDelete      = 201,
+
+        PublishRelease          = 210,
+
+        LicenseCreate   = 300,
+        LicenseDisable  = 301,
+        LicenseEnable   = 302
+    }
+}
