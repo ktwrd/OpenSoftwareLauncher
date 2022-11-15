120e6f084aab0f01e7aa77b4ca003440b1448139
kate <kate@dariox.club>
2022-11-15 14:08:01 +0800

    Add more AuditTypes for Announcements

--- OSLCommon/Logging/AuditType.cs
+++ OSLCommon/Logging/AuditType.cs
@@ -19,6 +19,8 @@ namespace OSLCommon.Logging
 
         AnnouncementModify      = 200,
         AnnouncementDelete      = 201,
+        AnnouncementCreate      = 202,
+        AnnouncementStateToggle = 203,
 
         PublishRelease          = 210,
 
