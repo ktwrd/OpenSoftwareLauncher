be15e2182561cf6b94e739134ef9e9f901747c0f
kate <kate@dariox.club>
2022-11-15 14:00:43 +0800

    Create entry data for AuditType.AccountLicenseUpdate

---
+++ OSLCommon/Logging/AccountLicenseUpdateEntryData.cs
@@ -0,0 +1,40 @@
+﻿using OSLCommon.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OSLCommon.Logging
+{
+    public class AccountLicenseUpdateEntryData : BaseEntryData
+    {
+        public AccountLicenseUpdateEntryData()
+            : base()
+        {
+            AuditType = AuditType.AccountLicenseUpdate;
+            Username = "";
+            Removed = Array.Empty<string>();
+            Added = Array.Empty<string>();
+        }
+
+        public AccountLicenseUpdateEntryData(Account account, string[] previous, string[] current)
+            : base()
+        {
+            AuditType = AuditType.AccountLicenseUpdate;
+            Username = account.Username;
+            List<string> removeList = new List<string>();
+            foreach (var item in previous)
+                if (!current.Contains(item))
+                    removeList.Add(item);
+            List<string> addList = new List<string>();
+            foreach (var item in current)
+                if (!previous.Contains(item))
+                    addList.Add(item);
+            Removed = removeList.ToArray();
+            Added = addList.ToArray();
+        }
+        public string Username { get; set; }
+        public string[] Removed { get; set; }
+        public string[] Added { get; set; }
+    }
+}
