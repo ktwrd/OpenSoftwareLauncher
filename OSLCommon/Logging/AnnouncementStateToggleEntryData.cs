8f6f15c2885dbcd690ff99ebbe610ac09e2127b0
kate <kate@dariox.club>
2022-11-15 14:49:05 +0800

    Create EntryData for AuditType.AnnouncementStateToggle

---
+++ OSLCommon/Logging/AnnouncementStateToggleEntryData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Logging
+{
+    public class AnnouncementStateToggleEntryData : BaseEntryData
+    {
+        public AnnouncementStateToggleEntryData()
+            : base()
+        {
+            AuditType = AuditType.AnnouncementStateToggle;
+            State = true;
+        }
+        public bool State { get; set; }
+    }
+}
