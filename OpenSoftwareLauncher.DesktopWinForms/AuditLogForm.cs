b49ff025f1997da7300dece0268daf1a8dfd13c2
kate <kate@dariox.club>
2022-11-16 10:03:46 +0800

    Add logic for redrawing listview and checkbox

--- OpenSoftwareLauncher.DesktopWinForms/AuditLogForm.cs
+++ OpenSoftwareLauncher.DesktopWinForms/AuditLogForm.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using kate.shared.Helpers;
+using OSLCommon;
+using OSLCommon.Logging;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -26,11 +31,111 @@ namespace OpenSoftwareLauncher.DesktopWinForms
                 item.Text = LocaleManager.Get(item.Text);
             }
             Text = LocaleManager.Get("Title_AuditLog");
+            Trace.WriteLine($"[AuditLogForm->Locale] Took {OSLHelper.GetMicroseconds() - start}µs");
         }
 
         private void AuditLogForm_Shown(object sender, EventArgs e)
         {
             Locale();
+        }
+
+        public void RedrawElements()
+        {
+            Trace.WriteLine($"[DEBUG] [AuditLogForm->RedrawElements]");
+            RedrawCheckboxTypes();
+        }
+        public void RedrawCheckboxTypes()
+        {
+            var items = GeneralHelper.GetEnumList<AuditType>();
+            var itemBlacklist = new AuditType[]
+            {
+                AuditType.None,
+                AuditType.Any
+            };
+            checkedListBoxTypes.Items.Clear();
+            foreach (var entry in items)
+            {
+                if (itemBlacklist.Contains(entry))
+                    continue;
+                checkedListBoxTypes.Items.Add(entry);
+            }
+            for (int i = 0; i < checkedListBoxTypes.Items.Count; i++)
+            {
+                AuditType checkboxItem = (AuditType)checkedListBoxTypes.Items[i];
+                checkedListBoxTypes.SetItemChecked(i, !TypeBlacklist.Contains(checkboxItem));
+            }
+            Trace.WriteLine($"[INFO] [AuditLogForm->RedrawCheckboxTypes] CheckedCount: {checkedListBoxTypes.CheckedItems.Count}");
+        }
+        public void RedrawListView()
+        {
+            listView1.Items.Clear();
+            foreach (var item in EntryListSorted)
+            {
+                var listViewItem = new ListViewItem(new string[]
+                {
+                    item.ActionType.ToString(),
+                    item.Username,
+                    DateTimeOffset.FromUnixTimeMilliseconds(item.Timestamp).LocalDateTime.ToString()
+                });
+                listViewItem.Name = item.UID;
+            }
+        }
+
+        public void UpdateFilter()
+        {
+            var sorted = new List<AuditLogEntry>();
+            foreach (var item in EntryList)
+            {
+                int min = 0;
+                int curr = 0;
+                if (UsernameFilter.Length > 0)
+                {
+                    min++;
+                    if (UsernameFilter.Contains(item.Username))
+                        curr++;
+                }
+                if (TypeBlacklist.Length > 0)
+                {
+                    min++;
+                    if (TypeBlacklist.Contains(item.ActionType))
+                        curr++;
+                }
+
+                if (min == curr)
+                    sorted.Add(item);
+            }
+            EntryListSorted = sorted.ToArray();
+            Trace.WriteLine($"[AuditLogForm->UpdateFilter] Diff Count: {EntryListSorted.Length - EntryList.Length}");
+            RedrawListView();
+        }
+
+        public void UpdateTypeBlacklist()
+        {
+            var blacklist = new List<AuditType>();
+            for (int i = 0; i < checkedListBoxTypes.Items.Count; i++)
+            {
+                var item = (AuditType)checkedListBoxTypes.Items[i];
+                if (checkedListBoxTypes.GetItemCheckState(i) == CheckState.Unchecked)
+                    blacklist.Add(item);
+            }
+            TypeBlacklist = blacklist.ToArray();
+            Trace.WriteLine($"[AuditLogForm->UpdateTypeBlacklist] Diff Count: {TypeBlacklist.Length - checkedListBoxTypes.Items.Count}");
+            UpdateFilter();
+        }
+
+        public AuditLogEntry[] EntryList = Array.Empty<AuditLogEntry>();
+        public AuditLogEntry[] EntryListSorted = Array.Empty<AuditLogEntry>();
+        public string[] UsernameFilter = Array.Empty<string>();
+        public AuditType[] TypeBlacklist = Array.Empty<AuditType>();
+
+        private void checkedListBoxTypes_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            UpdateTypeBlacklist();
+        }
+
+        private void toolStripButtonRefresh_Click(object sender, EventArgs e)
+        {
+            PullData();
         }
     }
 }
