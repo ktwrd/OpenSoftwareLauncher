65e4a22b9ce0651dfe67cbdcc0ca97437edb512a
kate <kate@dariox.club>
2022-11-17 10:19:29 +0800

    Create GenericDifference.cs

---
+++ OSLCommon/GenericDifference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace OSLCommon
+{
+    public class GenericDifference<T>
+    {
+        [Description("Previous state")]
+        public T Previous { get; set; }
+        [Description("Current State")]
+        public T Current { get; set; }
+    }
+}
