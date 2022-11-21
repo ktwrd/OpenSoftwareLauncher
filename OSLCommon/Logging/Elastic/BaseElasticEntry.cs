e394bf49514f590088163faa34b9ea052887dbc8
kate <kate@dariox.club>
2022-11-21 11:52:26 +0800

    Create class BaseElasticEntry

---
+++ OSLCommon/Logging/Elastic/BaseElasticEntry.cs
@@ -0,0 +1,20 @@
+﻿using kate.shared.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Logging.Elastic
+{
+    public class BaseElasticEntry
+    {
+        public BaseElasticEntry()
+        {
+            ID = GeneralHelper.GenerateUID();
+            Username = "<none>";
+            Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+        public string ID { get; set; }
+        public string Username { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
