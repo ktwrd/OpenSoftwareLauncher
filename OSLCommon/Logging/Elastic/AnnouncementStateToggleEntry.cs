90580a500a33b75bcba3154aae91af5f4f9f22a6
kate <kate@dariox.club>
2022-11-21 11:52:39 +0800

    Create elastic data class for AnnouncementStateToggle

---
+++ OSLCommon/Logging/Elastic/AnnouncementStateToggleEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Logging.Elastic
+{
+    public class AnnouncementStateToggleEntry : BaseElasticEntry
+    {
+        public AnnouncementStateToggleEntry()
+            : base()
+        {
+            State = false;
+        }
+        public AnnouncementStateToggleEntry(AnnouncementStateToggleEntryData data)
+            : base()
+        {
+            State = data.State;
+        }
+        public bool State { get; set; }
+    }
+}
