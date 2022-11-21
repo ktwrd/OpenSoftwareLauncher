4b69d45e6c29ed726f46bd9e55efe09b779d275b
kate <kate@dariox.club>
2022-11-21 16:29:45 +0800

    Update AssemblyInfo.cs

--- OpenSoftwareLauncher.DesktopWinForms/Properties/AssemblyInfo.cs
+++ OpenSoftwareLauncher.DesktopWinForms/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenSoftwareLauncher.DesktopWinForms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OSL Desktop")]
+[assembly: AssemblyDescription("Administration application for OSL Server")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenSoftwareLauncher.DesktopWinForms")]
+[assembly: AssemblyProduct("Open Software Launcher Desktop Panel")]
 [assembly: AssemblyCopyright("Copyright © Kate Ward 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.2.0")]
 [assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: NeutralResourcesLanguage("en")]
