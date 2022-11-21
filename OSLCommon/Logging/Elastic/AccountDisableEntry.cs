4d6d3742ed1036df1d059b31ad03cf6a5c8845c1
kate <kate@dariox.club>
2022-11-21 11:54:52 +0800

    Create elastic data class for AccountDisable

---
+++ OSLCommon/Logging/Elastic/AccountDisableEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Logging.Elastic
+{
+    public class AccountDisableEntry : BaseElasticEntry
+    {
+        public AccountDisableEntry()
+            : base()
+        {
+            TargetUsername = "";
+            State = false;
+            Reason = "";
+        }
+        public AccountDisableEntry(AccountDisableEntryData data)
+            : this()
+        {
+            TargetUsername = data.Username;
+            State = data.State;
+            Reason = data.Reason;
+        }
+
+        public string TargetUsername { get; set; }
+        public bool State { get; set; }
+        public string Reason { get; set; }
+    }
+}
