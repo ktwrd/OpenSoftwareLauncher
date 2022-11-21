cca79e82606b1fdf8e5a518bea86323e0be1beaf
kate <kate@dariox.club>
2022-11-21 13:55:01 +0800

    Create ServiceAccountCreateRequest.cs

---
+++ OSLCommon/Authorization/ServiceAccountCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSLCommon.Authorization
+{
+    public class ServiceAccountCreateRequest
+    {
+        public string Usernamme { get; set; }
+        public AccountPermission[] Permissions { get; set; }
+        public string[] Licenses { get; set; }
+    }
+}
