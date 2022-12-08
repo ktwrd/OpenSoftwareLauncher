05738a39aef4febbdc50f71403f00120ff058820
kate <kate@dariox.club>
2022-12-08 09:33:43 +0800

    Create TextBoxForm (it's a modal :3)

---
+++ OpenSoftwareLauncher.DesktopWinForms/TextBoxForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace OpenSoftwareLauncher.DesktopWinForms
+{
+    partial class TextBoxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonIgnore = new System.Windows.Forms.Button();
+            this.buttonRetry = new System.Windows.Forms.Button();
+            this.buttonAbort = new System.Windows.Forms.Button();
+            this.buttonNo = new System.Windows.Forms.Button();
+            this.buttonYes = new System.Windows.Forms.Button();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxContent, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(300, 80);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(302, 82);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.buttonCancel);
+            this.flowLayoutPanel1.Controls.Add(this.buttonOk);
+            this.flowLayoutPanel1.Controls.Add(this.buttonIgnore);
+            this.flowLayoutPanel1.Controls.Add(this.buttonRetry);
+            this.flowLayoutPanel1.Controls.Add(this.buttonAbort);
+            this.flowLayoutPanel1.Controls.Add(this.buttonNo);
+            this.flowLayoutPanel1.Controls.Add(this.buttonYes);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 51);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(302, 87);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(224, 3);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Visible = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(143, 3);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Visible = false;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonIgnore
+            // 
+            this.buttonIgnore.Location = new System.Drawing.Point(62, 3);
+            this.buttonIgnore.Name = "buttonIgnore";
+            this.buttonIgnore.Size = new System.Drawing.Size(75, 23);
+            this.buttonIgnore.TabIndex = 2;
+            this.buttonIgnore.Text = "Ignore";
+            this.buttonIgnore.UseVisualStyleBackColor = true;
+            this.buttonIgnore.Visible = false;
+            this.buttonIgnore.Click += new System.EventHandler(this.buttonIgnore_Click);
+            // 
+            // buttonRetry
+            // 
+            this.buttonRetry.Location = new System.Drawing.Point(224, 32);
+            this.buttonRetry.Name = "buttonRetry";
+            this.buttonRetry.Size = new System.Drawing.Size(75, 23);
+            this.buttonRetry.TabIndex = 3;
+            this.buttonRetry.Text = "Retry";
+            this.buttonRetry.UseVisualStyleBackColor = true;
+            this.buttonRetry.Visible = false;
+            this.buttonRetry.Click += new System.EventHandler(this.buttonRetry_Click);
+            // 
+            // buttonAbort
+            // 
+            this.buttonAbort.Location = new System.Drawing.Point(143, 32);
+            this.buttonAbort.Name = "buttonAbort";
+            this.buttonAbort.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbort.TabIndex = 4;
+            this.buttonAbort.Text = "Abort";
+            this.buttonAbort.UseVisualStyleBackColor = true;
+            this.buttonAbort.Visible = false;
+            this.buttonAbort.Click += new System.EventHandler(this.buttonAbort_Click);
+            // 
+            // buttonNo
+            // 
+            this.buttonNo.Location = new System.Drawing.Point(62, 32);
+            this.buttonNo.Name = "buttonNo";
+            this.buttonNo.Size = new System.Drawing.Size(75, 23);
+            this.buttonNo.TabIndex = 5;
+            this.buttonNo.Text = "No";
+            this.buttonNo.UseVisualStyleBackColor = true;
+            this.buttonNo.Visible = false;
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Location = new System.Drawing.Point(224, 61);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(75, 23);
+            this.buttonYes.TabIndex = 6;
+            this.buttonYes.Text = "Yes";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Visible = false;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
+            // 
+            // textBoxContent
+            // 
+            this.textBoxContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxContent.Location = new System.Drawing.Point(3, 28);
+            this.textBoxContent.MinimumSize = new System.Drawing.Size(4, 20);
+            this.textBoxContent.Name = "textBoxContent";
+            this.textBoxContent.Size = new System.Drawing.Size(296, 20);
+            this.textBoxContent.TabIndex = 1;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(3, 3);
+            this.label.Margin = new System.Windows.Forms.Padding(3);
+            this.label.Name = "label";
+            this.label.Padding = new System.Windows.Forms.Padding(3);
+            this.label.Size = new System.Drawing.Size(35, 19);
+            this.label.TabIndex = 2;
+            this.label.Text = "label";
+            // 
+            // TextBoxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 106);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(332, 135);
+            this.Name = "TextBoxForm";
+            this.Padding = new System.Windows.Forms.Padding(12);
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TextBoxForm_FormClosing);
+            this.Shown += new System.EventHandler(this.TextBoxForm_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        public System.Windows.Forms.Button buttonOk;
+        public System.Windows.Forms.TextBox textBoxContent;
+        public System.Windows.Forms.Label label;
+        public System.Windows.Forms.Button buttonCancel;
+        public System.Windows.Forms.Button buttonIgnore;
+        public System.Windows.Forms.Button buttonRetry;
+        public System.Windows.Forms.Button buttonAbort;
+        public System.Windows.Forms.Button buttonNo;
+        public System.Windows.Forms.Button buttonYes;
+    }
+}
---
+++ OpenSoftwareLauncher.DesktopWinForms/TextBoxForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenSoftwareLauncher.DesktopWinForms
+{
+    public partial class TextBoxForm : Form
+    {
+        public TextBoxForm()
+        {
+            InitializeComponent();
+            Buttons = MessageBoxButtons.OK;
+            EnableLabel = false;
+        }
+        public TextBoxForm(MessageBoxButtons buttons = MessageBoxButtons.OK, bool labelEnable = false, string label = "", string title = "")
+        {
+            InitializeComponent();
+            Buttons = buttons;
+            EnableLabel = labelEnable;
+            Text = title;
+            this.label.Text = label;
+        }
+
+        public void Locale()
+        {
+            // Only update locale of buttons
+            FormHelper.LocaleControl(this.flowLayoutPanel1.Controls);
+        }
+
+        public delegate void DialogResultDelegate(DialogResult result);
+        public event DialogResultDelegate Done;
+        private bool HasDone = false;
+        private void OnDone(DialogResult result)
+        {
+            if (Done != null)
+                Done?.Invoke(result);
+            HasDone = true;
+            Close();
+        }
+
+        public bool EnableLabel
+        {
+            get
+            {
+                return tableLayoutPanel1.RowStyles[0].SizeType == SizeType.AutoSize;
+            }
+            set
+            {
+                if (value)
+                {
+                    tableLayoutPanel1.RowStyles[0].SizeType = SizeType.Absolute;
+                    tableLayoutPanel1.RowStyles[0].Height = 0;
+                }
+                else
+                {
+                    tableLayoutPanel1.RowStyles[0].SizeType = SizeType.AutoSize;
+                }
+            }
+        }
+        private MessageBoxButtons _buttons = MessageBoxButtons.OK;
+        public MessageBoxButtons Buttons
+        {
+            get => _buttons;
+            set
+            {
+                _buttons = value;
+                buttonCancel.Visible = false;
+                buttonOk.Visible = false;
+                buttonAbort.Visible = false;
+                buttonRetry.Visible = false;
+                buttonIgnore.Visible = false;
+                buttonNo.Visible = false;
+                buttonYes.Visible = false;
+                switch (value)
+                {
+                    case MessageBoxButtons.OKCancel:
+                        buttonCancel.Visible = true;
+                        buttonOk.Visible = true;
+                        break;
+                    case MessageBoxButtons.OK:
+                        buttonOk.Visible = true;
+                        break;
+                    case MessageBoxButtons.AbortRetryIgnore:
+                        buttonAbort.Visible = true;
+                        buttonRetry.Visible = true;
+                        buttonIgnore.Visible = true;
+                        break;
+                    case MessageBoxButtons.YesNoCancel:
+                        buttonCancel.Visible = true;
+                        buttonNo.Visible = true;
+                        buttonYes.Visible = true;
+                        break;
+                    case MessageBoxButtons.YesNo:
+                        buttonNo.Visible = true;
+                        buttonYes.Visible = true;
+                        break;
+                    case MessageBoxButtons.RetryCancel:
+                        buttonCancel.Visible = true;
+                        buttonRetry.Visible = true;
+                        break;
+                }
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            OnDone(DialogResult.Cancel);
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            OnDone(DialogResult.OK);
+        }
+
+        private void buttonIgnore_Click(object sender, EventArgs e)
+        {
+            OnDone(DialogResult.Ignore);
+        }
+
+        private void buttonRetry_Click(object sender, EventArgs e)
+        {
+            OnDone(DialogResult.Retry);
+        }
+
+        private void buttonAbort_Click(object sender, EventArgs e)
+        {
+            OnDone(DialogResult.Abort);
+        }
+
+        private void buttonNo_Click(object sender, EventArgs e)
+        {
+            OnDone(DialogResult.No);
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            OnDone(DialogResult.Yes);
+        }
+
+        private void TextBoxForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!HasDone)
+                OnDone(DialogResult.None);
+        }
+
+        private void TextBoxForm_Shown(object sender, EventArgs e)
+        {
+            Locale();
+        }
+    }
+}
