31ca2eb9595965c0c64c1579bd19947cacf473dc
kate! <kate@dariox.club>
2022-12-19 21:19:17 +0800

    Create ServerHelper

---
+++ OpenSoftwareLauncher.Server/ServerHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace OpenSoftwareLauncher.Server
+{
+    public static class ServerHelper
+    {
+        public static string FindClientAddress(HttpContext context)
+        {
+            string address = context.Connection.RemoteIpAddress?.ToString() ?? "0.0.0.0";
+
+            string[] possibleHeaders = new string[]
+            {
+                "X-Forwarded-For",
+                "X-Real-IP"
+            };
+
+            foreach (var item in possibleHeaders)
+                if (context.Request.Headers.ContainsKey(item))
+                {
+                    address = context.Request.Headers[item];
+                    break;
+                }
+
+            return address;
+        }
+    }
+}
